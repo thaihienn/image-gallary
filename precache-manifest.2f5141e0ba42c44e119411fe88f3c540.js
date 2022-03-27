209ea20ba8cca8f3cb9784707cf8ceb185aceadc
thaihienn <hiendt0503@gmail.com>
2022-03-28 00:55:05 +0700

    Updates

---
+++ precache-manifest.2f5141e0ba42c44e119411fe88f3c540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b430559d566475cf5a11bab659b0a345",
+    "url": "/image-gallary/index.html"
+  },
+  {
+    "revision": "d23ac7f33dbd1b0448a6",
+    "url": "/image-gallary/static/css/main.e7ccdc72.chunk.css"
+  },
+  {
+    "revision": "7fb5f7a2e25a89d18022",
+    "url": "/image-gallary/static/js/2.500323c1.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/image-gallary/static/js/2.500323c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23ac7f33dbd1b0448a6",
+    "url": "/image-gallary/static/js/main.282684c6.chunk.js"
+  },
+  {
+    "revision": "ac9d85c64fc9f0ba0166",
+    "url": "/image-gallary/static/js/runtime-main.d8c43c0d.js"
+  }
+]);
